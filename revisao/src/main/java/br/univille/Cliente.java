975eefe45fecf375b84f28da3352d4dc2283c7bc
Rhuan471 <rhuanjosevoltolini@gmail.com>
2025-11-10 17:45:18 -0300

    fabsoftrhu

---
+++ revisao/src/main/java/br/univille/Cliente.java
@@ -0,0 +1,15 @@
+package br.univille;
+
+public class Cliente {
+    //variável - atributo
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
