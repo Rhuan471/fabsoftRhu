7a79f763ee8942224be343f607c00d5c3e43aee9
Rhuan471 <rhuanjosevoltolini@gmail.com>
2025-11-12 12:08:29 -0300

    fabsoftrhu

--- revisao/src/main/java/br/univille/Cliente.java
+++ revisao/src/main/java/br/univille/Cliente.java
@@ -4,6 +4,24 @@ public class Cliente {
     //variável - atributo
     private String nome;
 
+    //construtor - mesmo nome da classe e não tem retorno
+    // iniciallizar atributos
+    // obrigar a passagem de valores
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    //polimorfismo - várias formas de existir a mesma coisa (métodos e construtores)
+    public Cliente() {
+        
+    }
+
+    @Override
+    //toda vez que pega um objeto que não é texto (String) e tenta transformar em texto automaticamente,
+    //o Java chama esse método toString()
+    public String toString() {
+        return getNome();
+    }
+    //método
     public String getNome() {
         return nome;
     }
@@ -11,5 +29,7 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    //encapsulamento = esconder os atributos (variáveis) da classe, ou seja,
+    //implementação dentro do objeto
 
 }
--- revisao/src/main/java/br/univille/Main.java
+++ revisao/src/main/java/br/univille/Main.java
@@ -3,9 +3,13 @@ package br.univille;
 public class Main {
     public static void main(String[] args) {
         
-        Cliente zezinho = new Cliente();
+        Cliente zezinho = new Cliente("Zezinho");
         zezinho.setNome("Zezinho da Silva");
-        Cliente mariazinha = new Cliente();
+        Cliente mariazinha = new Cliente("Mariazinha");
         mariazinha.setNome("Mariazinha de Souza");
+
+
+        System.out.println(zezinho);
+        System.out.println(mariazinha);
     }
 }
